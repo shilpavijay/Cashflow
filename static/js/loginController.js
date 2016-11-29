bcdf5ca3637054e2ddd057434e26dc84accaca35
shilpavijay <shilpa.amberker@gmail.com>
2016-11-29 21:42:25 +0530

    loginpg validations completed

--- static/js/loginController.js
+++ static/js/loginController.js
@@ -1,4 +1,23 @@
 
-var app = angular.module("loginApp",[])
-.controller("loginController",['$scope','$http', function($scope,$http){
-}]);
+var app = angular.module("loginApp",[]);
+
+// app.directive('validationDir',function(){
+// 	return {
+// 		require: 'ngModel',
+// 		link: function (scope,elm,attrs,ctrl) {
+// 				ctrl.$parsers.unshift(function(viewValue, $scope) {
+
+// 		        var noMatch = viewValue != scope.reference
+// 		        ctrl.$setValidity('noMatch', !noMatch);
+// 		        return (noMatch)?noMatch:!noMatch;
+// 			      });
+
+// 			    scope.$watch("reference", function(value) {
+// 		        ctrl.$setValidity('noMatch', value === ctrl.$viewValue);
+// 				})
+// 		}
+// 	}
+// });
+
+app.controller("loginController",['$scope','$http', function($scope,$http){
+}]);			    
